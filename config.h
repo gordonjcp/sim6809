d9e945b94ba024e5f8ef7982ac82620e8c710730
Gordon JC Pearce <gordon@gjcp.net>
2016-06-28 23:45:52 +0100

    started on autotools

--- config.h
+++ config.h
@@ -1,57 +1,60 @@
-/* config.h -- Configuration file
-   Copyright (C) 1998 Jerome Thoen
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
 
-/* 
- * You MUST define theses data types
- */
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
 
-/*
- * Define here your 8 bits types
- *
- */
-typedef unsigned char tt_u8;
-typedef signed char tt_s8;
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
 
-/*
- * Define here your 16 bits types 
- *
- */
-typedef unsigned short tt_u16;
-typedef signed short tt_s16;
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
 
-/*
- * Define here your 32 bits types
- *
- */
-typedef unsigned long tt_u32;
-typedef signed long tt_s32;
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
 
-/*
- * define to keep a PC history in debugger
- */
-#define PC_HISTORY
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
 
-/*
- * define size of PC buffer
- * must be defined only if PC_HISTORY is defined
- */
-#define PC_HISTORY_SIZE 20
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
 
-/*
- * define to compute CC V bit only when required
- */
-#define BIT_V_DELAYED
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "gordon@gjcp.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "sim6809"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "sim6809 0.1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "sim6809"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
